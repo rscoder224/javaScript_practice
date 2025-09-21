a5470b9d6bd6a52810c1823723868d3ad15da803
Lalit Tiwari <ltsmartofficial@gmail.com>
2025-09-21 20:14:59 +0530

    Create 0001.js

---
+++ 0001.js
@@ -0,0 +1,37 @@
+
+
+class Person{
+    constructor(firstName,lastName){
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+    
+    about(){
+        console.log(`Your fullname is ${this.firstName} ${this.lastName} `);
+    }
+    
+    get fullName(){
+        return `${this.firstName} ${this.lastName}`;
+    }
+    
+    set fullName(fullName){
+        let[firstName,lastName] = fullName.split(" ");
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+    
+}
+
+
+
+const user1 = new Person("Shaurya","Tiwari");
+
+console.log(user1);
+
+
+console.log(user1.fullName);
+
+user1.fullName = "Kallu Tiwari";
+
+console.log(user1.fullName);
+
